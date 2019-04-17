ab0012462cf3d497bd5be143454a93117f4faef5
Joseph Hayes <objectivejoe@gmail.com>
2019-04-17 09:21:42 -0700

    Sample Program for passing large lists of strings

---
+++ FizzBuzz/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using FizzBuzz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = System.IO.Path.GetTempFileName();
+            FileStream fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Write | FileAccess.Read);
+
+            int min = 1;
+            int max = int.MaxValue;
+
+            Dictionary<int, String> numberNameMap = new Dictionary<int, string> { { 3, "three" }, { 5, "five"}, { 7, "seven" }, { 10, "ten" } };
+
+            Fizz.TokenizeMultiples(min, max, numberNameMap, fs);
+
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                sr.BaseStream.Seek(0, SeekOrigin.Begin);
+                string str = null;
+                while ((str = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Fizz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FizzBuzz
+{
+    public static class Fizz
+    {
+        public static void TokenizeMultiples(int min, int max, Dictionary<int, String> numberNameMap, FileStream fs)
+        {
+            if (min >= max)
+            {
+                throw new System.ArgumentException("Min parameter must be less than max Paramenter");
+            }
+
+            StreamWriter sw = new StreamWriter(fs);
+            sw.BaseStream.Seek(0, SeekOrigin.End);
+
+            for (int i = min; i <= max; i++)
+            {
+                if (i < min)
+                    break;
+
+                string numToken = string.Empty;
+
+                foreach (KeyValuePair<int, string> entry in numberNameMap)
+                {
+                    if (i % entry.Key == 0)
+                    {
+                        numToken = numToken + entry.Value + " ";
+                    }
+                }
+
+                if (numToken == String.Empty)
+                {
+                    numToken = i.ToString();
+                }
+                sw.WriteLine("{0}", numToken);
+            }
+
+            sw.Flush();
+        }
+    }
+}
